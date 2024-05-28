ccc38cbbf2b9593f607493475a75e90c0695bab4
persistisok <101549910+persistisok@users.noreply.github.com>
2024-05-28 20:31:25 +0800

    debug: multiprocessing_context

--- utils/regression_trainer.py
+++ utils/regression_trainer.py
@@ -58,7 +58,8 @@ class RegTrainer(Trainer):
                                           if x == 'train' else 1),
                                           shuffle=(True if x == 'train' else False),
                                           num_workers=args.num_workers*self.device_count,
-                                          pin_memory=(True if x == 'train' else False))
+                                          pin_memory=(True if x == 'train' else False),
+                                          multiprocessing_context='spawn')
                             for x in ['train', 'val', 'test']}
 
         self.model = ThermalRGBNet(args)
