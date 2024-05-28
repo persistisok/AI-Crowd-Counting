e4c7e95c5f8ee1819497dee3c19a1b638b74672d
persistisok <101549910+persistisok@users.noreply.github.com>
2024-05-28 18:36:04 +0800

    modify: preprocess dataset

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 python test.py \
     --data-dir ./preprocessed-dataset-RGBTCC-CVPR2021 \
-    --save-dir /output/model \
+    --save-dir ./output/model \
     --model "model_name" \
     --device 0
