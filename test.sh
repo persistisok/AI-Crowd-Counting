2728bb09356877868230f0f90fce9329dc851b69
persistisok <101549910+persistisok@users.noreply.github.com>
2024-05-30 08:06:32 +0800

    for debug

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 python test.py \
     --data-dir ./preprocessed-dataset/ \
-    --save-dir ./output/model/0529-164042 \
+    --save-dir ./output/model/0529-234135 \
     --model "best_model.pth" \
     --device 0
